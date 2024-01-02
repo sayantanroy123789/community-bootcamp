57d07cf601c6cec7d050359b4313c214ae34f101
sayantan <sayantan.roy123789@gmail.com>
2024-01-03 00:49:30 +0530

    first commit

---
+++ Community/src/FirstTask.java
@@ -0,0 +1,7 @@
+public class FirstTask {
+    public static void main(String[] args) {
+        System.out.println("welocme");
+        System.out.println("java");
+        System.out.println("ok");
+    }
+}
---
+++ Community/src/Input.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter input:");
+        int roll=input.nextInt();
+        System.out.println("your roll number is "+roll);
+
+    }
+}

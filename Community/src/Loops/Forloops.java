a38f6a5094ee0afced68188183a37f846e8ae133
sayantan <sayantan.roy123789@gmail.com>
2024-01-03 16:28:31 +0530

    for loops

---
+++ Community/src/Loops/Forloops.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class Forloops {
+    public static void main(String[] args) {
+        for(int num=1;num<=5;num++){
+            System.out.println(num);
+        }
+    }
+    
+}

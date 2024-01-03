e5eeba26232393bb91e2c9a930a807eaa3d0cb53
sayantan <sayantan.roy123789@gmail.com>
2024-01-03 16:42:09 +0530

    for loop

--- Community/src/Loops/Forloops.java
+++ Community/src/Loops/Forloops.java
@@ -1,10 +1,19 @@
 package Loops;
-
+import java.util.Scanner;
 public class Forloops {
     public static void main(String[] args) {
         for(int num=1;num<=5;num++){
             System.out.println(num);
         }
+        //user input
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the renge:");
+        int num=input.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.print(i+" ");
+            
+        }
+
     }
     
 }
